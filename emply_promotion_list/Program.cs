68fec867608d7ce0315b8623acac392bfafc1f35
Bavi <bavidhraa.shrrei@kanini.com>
2021-01-27 23:18:23 +0530

    Add project files.

---
+++ emply_promotion_list/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emply_promotion_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employeedetails em = new Employeedetails();
+            Console.WriteLine("Please enter the employee names in the order of their eligibility for promotion");
+            em.Add_employee_name();
+            Console.WriteLine("Promoted employee list:");
+            em.sort_list();
+            em.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ emply_promotion_list/emplydetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emply_promotion_list
+{
+    public class Employeedetails
+    {
+        List<string> emp_name = new List<string>();
+        string name;
+        public void Add_employee_name()
+        {
+            do
+            {
+                name = Console.ReadLine();
+                emp_name.Add(name);
+            } while (!string.IsNullOrEmpty(name));
+        }
+        public void sort_list()
+        {
+            emp_name = emp_name.OrderBy(n => n).ToList();
+
+        }
+        public void Display()
+        {
+            foreach(string name in emp_name)
+            {
+                Console.WriteLine("{0}", name);
+            }
+        }
+    }
+}
